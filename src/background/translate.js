b51e8f7da6f1ff5590ed25fccaa6c8fd5204792e
fuatkarakus <fuatkrks11@gmail.com>
2020-01-20 02:42:19 +0300

    trials

--- src/background/translate.js
+++ src/background/translate.js
@@ -137,3 +137,61 @@ function tabCreateWithOpenerTabId(uri, tab) {
         chrome.tabs.create(newTabConfig);
     });
 }
+
+var bubbleDOM = document.createElement('div');
+bubbleDOM.setAttribute('class', 'selection_bubble');
+document.body.appendChild(bubbleDOM);
+
+// Lets listen to mouseup DOM events. 
+document.addEventListener('mouseup', function (e) {
+  var selection = window.getSelection().toString();
+  if (selection) {
+    if (selection.length >= 2) {
+      // console.log("clientX: " + e.clientX + ", clientX: " + e.clientY);
+      console.log("selection : " + selection);
+      renderBubble(e.pageX + 3, e.pageY + 3, selection);
+    }
+  }
+}, false);
+window.addEventListener('load', function () {
+    let selectedText = info.selectionText;
+    let config = Config.config;
+    var link = document.getElementsByClassName('selection_bubble');
+    link.addEventListener('click', function(e) {
+        console.log("icon clicked");
+        openTranslate(config.translateURL + encodeURIComponent(selectedText), tab);
+    });
+});
+
+chrome.runtime.onMessage.addListener(function (message, sender, callback) {
+  if (message.url) {
+      console.log(message);
+      show_modal(message.url, message.fullscreen);
+  }
+});
+
+// Close the bubble when we click on the screen.
+document.addEventListener('mousedown', function (e) {
+  bubbleDOM.style.visibility = 'hidden';
+}, false);
+
+// Move that bubble to the appropriate location.
+function renderBubble(mouseX, mouseY, selection) {
+  // bubbleDOM.innerHTML = selection;
+  bubbleDOM.style.top = mouseY + 'px';
+  bubbleDOM.style.left = mouseX + 'px';
+  bubbleDOM.style.visibility = 'visible';
+}
+
+function show_modal(url, fullscreen = false) {
+  let agentMatch = window.navigator.userAgent.match(/Firefox\/([0-9]+)\./);
+  let firefoxVersion = agentMatch ? parseInt(agentMatch[1]) : 0;
+
+  let tgt_model = firefoxVersion > 63
+      ? new Modal({url: url, fullscreen: fullscreen}, false)
+      : new ModalIframe({url: url, fullscreen: fullscreen}, false);
+
+  tgt_model.build().then(() => {
+      tgt_model.show();
+  });
+}
--- src/content/bubble.js
+++ src/content/bubble.js
@@ -1,28 +1,49 @@
-var bubbleDOM = document.createElement('div');
-bubbleDOM.setAttribute('class', 'selection_bubble');
-document.body.appendChild(bubbleDOM);
+// var bubbleDOM = document.createElement('div');
+// bubbleDOM.setAttribute('class', 'selection_bubble');
+// document.body.appendChild(bubbleDOM);
 
-// Lets listen to mouseup DOM events.
-document.addEventListener('mouseup', function (e) {
-  var selection = window.getSelection().toString();
-  if (selection) {
-    if (selection.length >= 2) {
-      // console.log("clientX: " + e.clientX + ", clientX: " + e.clientY);
-      console.log("selection : " + selection);
-      renderBubble(e.pageX, e.pageY, selection);
-    }
-  }
-}, false);
+// // Lets listen to mouseup DOM events. 
+// document.addEventListener('mouseup', function (e) {
+//   var selection = window.getSelection().toString();
+//   if (selection) {
+//     if (selection.length >= 2) {
+//       // console.log("clientX: " + e.clientX + ", clientX: " + e.clientY);
+//       console.log("selection : " + selection);
+//       renderBubble(e.pageX + 3, e.pageY + 3, selection);
+//     }
+//   }
+// }, false);
 
-// Close the bubble when we click on the screen.
-document.addEventListener('mousedown', function (e) {
-  bubbleDOM.style.visibility = 'hidden';
-}, false);
 
-// Move that bubble to the appropriate location.
-function renderBubble(mouseX, mouseY, selection) {
-  // bubbleDOM.innerHTML = selection;
-  bubbleDOM.style.top = mouseY + 'px';
-  bubbleDOM.style.left = mouseX + 'px';
-  bubbleDOM.style.visibility = 'visible';
-}
+// chrome.runtime.onMessage.addListener(function (message, sender, callback) {
+//   if (message.url) {
+//       console.log(message);
+//       show_modal(message.url, message.fullscreen);
+//   }
+// });
+
+// // Close the bubble when we click on the screen.
+// document.addEventListener('mousedown', function (e) {
+//   bubbleDOM.style.visibility = 'hidden';
+// }, false);
+
+// // Move that bubble to the appropriate location.
+// function renderBubble(mouseX, mouseY, selection) {
+//   // bubbleDOM.innerHTML = selection;
+//   bubbleDOM.style.top = mouseY + 'px';
+//   bubbleDOM.style.left = mouseX + 'px';
+//   bubbleDOM.style.visibility = 'visible';
+// }
+
+// function show_modal(url, fullscreen = false) {
+//   let agentMatch = window.navigator.userAgent.match(/Firefox\/([0-9]+)\./);
+//   let firefoxVersion = agentMatch ? parseInt(agentMatch[1]) : 0;
+
+//   let tgt_model = firefoxVersion > 63
+//       ? new Modal({url: url, fullscreen: fullscreen}, false)
+//       : new ModalIframe({url: url, fullscreen: fullscreen}, false);
+
+//   tgt_model.build().then(() => {
+//       tgt_model.show();
+//   });
+// }
