864a26e9e54c71d5aeccc550403ccf37242b3757
fuatkarakus <fuatkrks11@gmail.com>
2020-02-25 17:55:27 +0300

    event conflicts resolved

--- src/content/bubble.js
+++ src/content/bubble.js
@@ -1,42 +1,44 @@
 var bubbleDOM = document.createElement('div');
 bubbleDOM.setAttribute('class', 'selection_bubble');
 document.body.appendChild(bubbleDOM);
-var globalSelection;
-// Lets listen to mouseup DOM events. 
-document.addEventListener('mouseup', function (e) {
-  var selection = window.getSelection().toString();
-  if (selection) {
-    if (selection.length >= 2) {
-      // console.log("clientX: " + e.clientX + ", clientX: " + e.clientY);
-      // console.log("selection : " + selection);
-      renderBubble(e.pageX + 3, e.pageY + 3, selection);
-      globalSelection = selection;
-      console.log("selection :  " + globalSelection);
-    }
+var selection = "";
+function getSelectionText() {
+  var text = "";
+  if (window.getSelection) {
+    text = window.getSelection().toString();
+  } else if (document.selection && document.selection.type != "Control") {
+    text = document.selection.createRange().text;
   }
-}, false);
+  return text;
+}
+document.addEventListener("mousedown", checkSelectionText);
+document.addEventListener("mouseup", checkSelectionText);
 
-// document.addEventListener('mousedown', function (e) {
-//   console.log("mause down");
-//   bubbleDOM.style.visibility = 'hidden';
-// }, false);
+function checkSelectionText(event) {
+  if (event.target == document.querySelector('.selection_bubble')) {
+    return;
+  }
 
-document.querySelector('.selection_bubble').addEventListener('click', function(e) {
-  chrome.runtime.sendMessage({action: 'openModal', selection: globalSelection});
-  bubbleDOM.style.visibility = 'hidden';
-  console.log("bubble selection: " + globalSelection);
-}, false);
-
-function clickHandler(){
-  chrome.runtime.sendMessage({action: 'openModal'});
-  bubbleDOM.style.visibility = 'hidden';
-  console.log("clicked");
+  setTimeout(function() {
+    selection = getSelectionText();
+    if (selection) {
+      console.log("checkSelectionText");
+      document.querySelector('.selection_bubble').style.top = (event.pageY+3) + "px";
+      document.querySelector('.selection_bubble').style.left = (event.pageX+3) + "px";
+      document.querySelector('.selection_bubble').style.visibility = 'visible';
+    } else {
+      document.querySelector('.selection_bubble').style.visibility = 'hidden';
+    }
+  });
 }
 
-// Move that bubble to the appropriate location.
-function renderBubble(mouseX, mouseY, selection) {
-  // bubbleDOM.innerHTML = selection;
-  bubbleDOM.style.top = mouseY + 'px';
-  bubbleDOM.style.left = mouseX + 'px';
-  bubbleDOM.style.visibility = 'visible';
+document.querySelector('.selection_bubble').addEventListener("click", openPopup);
+
+function openPopup(event) {
+  console.log( "openPopup " + selection);
+  chrome.runtime.sendMessage({action: 'openModal', selection: selection});
+  document.querySelector('.selection_bubble').style.visibility = 'hidden';
+  
+  event.preventDefault();
+  event.stopPropagation();
 }
