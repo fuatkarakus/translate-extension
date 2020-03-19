85e34e8d2919694b782a16bd72d0f4727f17f43c
fuatkarakus <fuatkrks11@gmail.com>
2020-03-20 00:41:12 +0300

    firefox add on id issue

--- src/content/bubble.js
+++ src/content/bubble.js
@@ -1,5 +1,5 @@
-var bubbleDOM = document.createElement('div');
-bubbleDOM.setAttribute('class', 'selection_bubble');
+var bubbleDOM = document.createElement("div");
+bubbleDOM.setAttribute("class", "selection_bubble");
 document.body.appendChild(bubbleDOM);
 var selection = "";
 function getSelectionText() {
@@ -15,28 +15,32 @@ document.addEventListener("mousedown", checkSelectionText);
 document.addEventListener("mouseup", checkSelectionText);
 
 function checkSelectionText(event) {
-  if (event.target == document.querySelector('.selection_bubble')) {
+  if (event.target == document.querySelector(".selection_bubble")) {
     return;
   }
 
-  setTimeout(function() {
+  setTimeout(function () {
     selection = getSelectionText();
     if (selection) {
-      document.querySelector('.selection_bubble').style.top = (event.pageY+3) + "px";
-      document.querySelector('.selection_bubble').style.left = (event.pageX+3) + "px";
-      document.querySelector('.selection_bubble').style.visibility = 'visible';
+      document.querySelector(".selection_bubble").style.top =
+        event.pageY + 3 + "px";
+      document.querySelector(".selection_bubble").style.left =
+        event.pageX + 3 + "px";
+      document.querySelector(".selection_bubble").style.visibility = "visible";
     } else {
-      document.querySelector('.selection_bubble').style.visibility = 'hidden';
+      document.querySelector(".selection_bubble").style.visibility = "hidden";
     }
   });
 }
 
-document.querySelector('.selection_bubble').addEventListener("click", openPopup);
+document
+  .querySelector(".selection_bubble")
+  .addEventListener("click", openPopup);
 
 function openPopup(event) {
-  chrome.runtime.sendMessage({action: 'openModal', selection: selection});
-  document.querySelector('.selection_bubble').style.visibility = 'hidden';
-  
+  chrome.runtime.sendMessage({ action: "openModal", selection: selection });
+  document.querySelector(".selection_bubble").style.visibility = "hidden";
+
   event.preventDefault();
   event.stopPropagation();
 }
